66add51a5f565efebb52f6d43e3248da74758d7e
Jeff.A <kinesthesiac@gmail.com>
2022-11-12 22:44:54 -0800

    finished the conditional statement

---
+++ ClassProjects/Web Development/JSmodule2/loveCalc2.js
@@ -0,0 +1,10 @@
+// instructor's example:
+
+var loveScore = Math.random() * 100
+loveScore = Math.floor(loveScore) + 1
+
+if (loveScore > 70) {
+    console.log("Your love score is " + loveScore + "%.  You love each other like Kanye loves Ye")
+} else {
+    console.log("Your love score is " + loveScore + "%.")
+}
