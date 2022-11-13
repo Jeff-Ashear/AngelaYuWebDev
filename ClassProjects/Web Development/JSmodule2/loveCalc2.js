211f66eccd3c209dd7ba012eca79ad8bd0577874
Jeff.A <kinesthesiac@gmail.com>
2022-11-13 00:04:06 -0800

    improved conditionals

--- ClassProjects/Web Development/JSmodule2/loveCalc2.js
+++ ClassProjects/Web Development/JSmodule2/loveCalc2.js
@@ -5,6 +5,8 @@ loveScore = Math.floor(loveScore) + 1
 
 if (loveScore > 70) {
     console.log("Your love score is " + loveScore + "%.  You love each other like Kanye loves Ye")
-} else {
+} else if (loveScore > 30 && loveScore <= 70) {
     console.log("Your love score is " + loveScore + "%.")
+} else {
+    console.log("Your love score is " + loveScore + "%.  Sorry.  You're going to be lonely.")
 }
