079c53876828c0ce7b7ae0fc897f9ed94be93bc9
Jeff Ashear <kinesthesiac@gmail.com>
2022-12-04 16:26:20 -0800

    lesson 132

--- ClassProjects/Web Development/JSmodule2/fizzbuzz.js
+++ ClassProjects/Web Development/JSmodule2/fizzbuzz.js
@@ -1,5 +1,21 @@
-let arrNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]
+let arrAuto = [];
 
+// produces and returns an array of numbers in order from 1 to 100
+function populator(arr) {
+    for (let i = 0; i < 100; i++) {
+        arrAuto.push(i + 1)
+    }
+    return arrAuto
+}
+
+// (populator(arrAuto))
+
+// scans the above array for:
+    // mulitples of 3 and 5, and then prints "FizzBuzz"
+    // multiples of 5, and then prints "Buzz"
+    // multiples of 3, and then prints "Fizz"
+    // in cases not meeting the condintions above the number itself is printed
+    
 function fizzBuzz(arr) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
@@ -14,4 +30,40 @@ function fizzBuzz(arr) {
     }
 }
 
-fizzBuzz(arrNums)
+// fizzBuzz(arrAuto)
+
+let arrManual = []
+
+function Bizzfuzz() {
+    if (arrManual == false) {
+        arrManual.push(1)
+    }
+
+     var num = arrManual.pop()
+
+     arrManual.push(num)
+     arrManual.push(num + 1)
+
+    console.log(arr)
+    return arrManual
+}
+
+// Bizzfuzz(arrManual)
+
+var output = []
+let count = 1
+
+function fizzBuzz() {
+    if (count % 3 ===0 && count % 5 === 0) {
+        output.push("FIzzBuzz")
+    } else if (count % 3 === 0) {
+        output.push("Fizz")
+    } else if (count % 5 === 0) {
+        output.push("Buzz:")
+    } else {
+        output.push(count)
+    }
+    count++
+    
+    return output 
+}
