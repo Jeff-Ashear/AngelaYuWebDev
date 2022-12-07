e91f1961a53ff262add3ae3fa59b5f0002440e72
Jeff Ashear <kinesthesiac@gmail.com>
2022-12-07 14:54:58 -0800

    lesson 134 pt1

---
+++ ClassProjects/Web Development/JSmodule2/fizzbuzz2.js
@@ -0,0 +1,22 @@
+let arr = [];
+
+function fizzBuzz(nums) {
+    if (arr === false) {
+        let i = 1
+        while (i > 101) {
+            i++
+            if (i % 3 === 0 && i % 5 === 0) {
+                arr.push("Fizzbuzz")
+            } else if (i % 3 === 0) {
+                arr.push("Fizz") 
+            } else if (i % 5 === 0) {
+                arr.push("Buzz")
+            } else {
+                arr.push(i)
+            }
+        }
+    }
+    return arr
+}
+
+console.log(fizzBuzz(arr))
