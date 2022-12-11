7ea2b83269307e52e209feb11dac6208073354e8
Jeff Ashear <kinesthesiac@gmail.com>
2022-12-10 23:05:18 -0800

    while loop practice

--- ClassProjects/Web Development/JSmodule2/99bottles.js
+++ ClassProjects/Web Development/JSmodule2/99bottles.js
@@ -3,20 +3,19 @@ let bottles = 99
 function numOnWall(num) {
     let bottleWord = "bottles"
 
-   while (bottles >= 0) {
-    if (bottles = 1) {
-        bottleWord = "bottle"
-    } else {
-        bottleWord = "bottles"
+    while (bottles >= 0) {        
+            console.log(bottles + " " + bottleWord + " of beer on the wall. " + bottles + " " + bottleWord + " of beer.")
+            bottles-- 
+            if (bottles === 1) {
+                bottleWord = "bottle"
+            }
+            if (bottles === 0) {
+                bottleWord = "bottles"
+                console.log("Go to the store. Buy some more. 99 bottles of beer on the wall.")
+            }
+            console.log("Take one down. Pass it around. " + bottles + " " + bottleWord + " of beet on the wall.")
     }
-    console.log(bottles + " " + bottleWord + " of beer on the wall," + bottles + " " + bottleWord + " of beer.")
-    bottles--
-    if (bottles > 0) {
-        console.log("Take one down, pass it around.  " + bottles + " " + bottleWord + " of beer on the wall.")    
-    } else if (bottles = 0) {
-        console.log("Go to the store, buy some more.  99 " + bottleWord + " of beer on the wall.")
-    }
-} 
+
 }
 
 numOnWall(bottles)
