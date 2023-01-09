7cc92e11f19fe97af7a790a66cbd6b209589fe36
Jeff Ashear <kinesthesiac@gmail.com>
2023-01-08 23:21:44 -0800

    nice clean 99 bottles

--- ClassProjects/Web Development/JSmodule2/99bottles.js
+++ ClassProjects/Web Development/JSmodule2/99bottles.js
@@ -1,14 +1,26 @@
 let bottlesNum = 99
-let bottleSSSS = "s"
+let bottles = "bottles"
 let line1 = ""
 let line2 = ""
 
 function lyrics() {
 
-    while (bottlesNum > 1) {
-        console.log(bottlesNum + " bottles of beer on the wall.  " + bottlesNum + " bottles of beer.")
-        bottlesNum--
-        console.log("Take one down, pass it around.  " + bottlesNum + " bottles of beer on the wall.")
+    function checkTheS(num) {
+        if (bottlesNum != 1) {
+            bottles = "bottles"
+        } else {
+            bottles = "bottle"
+        }
     }
 
+    while (bottlesNum >= 1) {
+        checkTheS(bottlesNum)
+        console.log(bottlesNum + " " + bottles + " of beer on the wall.  " + bottlesNum + " " + bottles + " of beer.")
+        bottlesNum--
+        checkTheS(bottlesNum)
+        console.log("Take one down, pass it around.  " + bottlesNum + " " + bottles + " of beer on the wall.")
+   
+    }
 }
+
+lyrics(bottlesNum)
